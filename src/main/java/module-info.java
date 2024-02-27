a7cbd9fe596f720a57f721ee1666d482c9f49f3f
tungduong028 <97148539+tungduong028@users.noreply.github.com>
2024-02-27 11:42:06 +0700

    Revert " Student branch "

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,9 @@
 module com.example.coursemanagement {
     requires javafx.controls;
     requires javafx.fxml;
-    requires lombok;
-    requires java.sql;
-    requires mysql.connector.j;
 
 
     opens com.example.coursemanagement.bll.dtos to javafx.base;
     opens com.example.coursemanagement to javafx.fxml;
     exports com.example.coursemanagement;
-    exports com.example.coursemanagement.gui.controller;
-    exports com.example.coursemanagement.gui.page;
-    opens com.example.coursemanagement.gui.controller to javafx.fxml;
 }
-///
