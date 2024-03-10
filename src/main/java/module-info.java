50109c726b45cf2d5858b2444f1512ebda9dff6d
SangNguyen <nngocsang38@gmail.com>
2024-03-10 18:55:13 +0700

    update 10/3

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.coursemanagement {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.coursemanagement.bll.dtos to javafx.base;
-    opens com.example.coursemanagement to javafx.fxml;
-    exports com.example.coursemanagement;
-}
